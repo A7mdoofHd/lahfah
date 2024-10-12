792fa71b43b86a820d32fc8c6c00cf20eaaf74cd
A7mdoofHd <a7med.mads@gmail.com>
2024-10-12 06:39:40 +0300

    Create lahfah.js

---
+++ lahfah.js
@@ -0,0 +1,108 @@
+$('<link rel="stylesheet" type="text/css" href="">\n<link rel="stylesheet" href="">\n<style>\n\n#wall .fitimg.u-pic.borderg {\nheight: 52px !important;\nborder: 1px solid;\nborder-radius: 100px ;\nbox-shadow: 0 0 0px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #707070;\nmargin-top: 2px;\nmargin-right: 2px;\n}\n\n#chats .fitimg.u-pic {\nheight: 52px !important;\nborder: 1px solid;\nborder-radius: 100px ;\nbox-shadow: 0 0 0px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #707070;\nmargin-left: -10px;\n}\n\n#chats img.ustat {\n    width: 11px !important;\n    height: 11px !important;\n    min-height: 0%!important;\n    margin-right: 0;\n\n    z-index: 999999;\n    border-radius: 50px;\n    margin-left: 1px;\n    margin-top: 1px;\n    border: 1px solid #000;\n    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #f0c01f;\nbox-shadow: 0 0 3px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #000;\n      \n    }\n\n#chats .d-flex.fl{background-color:#d4cfd2!important;background-image: url(https://up6.cc/2024/01/170570176928032.jpg);\n  background-size: cover;}\n\n\n#chats .mini.u-msg.dots{color:#000!important;}\n\n\n\n#chats{background-color:#d4cfd2!important;background-image: url(https://up6.cc/2024/01/170570176928453.jpg);\n  background-size: cover;}\n\n\n</style>').insertBefore("body");
+
+
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<style>
+
+
+
+span.flex-grow-1.dots {text-align: center;}
+.dots {white-space: normal!important;}
+span.btn.fr.fa.fa-eye {transform: matrix(1,0,0,1,-40,-3);}
+.fa.fa-user.label.badgex.label-success {transform: matrix(1,0,0,1,-55,0);}
+div#l1 input#u1,div#l3 button{width:98%!important;margin-left:3px}
+div#l2 button,div#l3 button{ background-image: linear-gradient(to left, rgba(255,255,255,0.2), rgba(99,121,125,15));border-radius:0px 5px 0px 5px!important;margin-left: 3px;border:1px solid #ccc!important;border-radius:2px!important;height:27px!important;padding:0 5px}
+div#l1 input#u1{ opacity: 0.5;height: 25px!important;border-radius:0px 4px 4px 4px!important;border:1px solid #000000;margin-top:3px!important;text-align:center}
+div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3{height: 23px!important;width:49%!important;padding:0!important;float:right;margin:2px 5px 2px 0}
+
+div#l2 input#u2,div#l3 input#u3{opacity: 0.5;margin-left:-6px;border-radius:0px 4px 4px 4px!important;border:1px solid #000000;text-align:center}
+div#l2 input#pass1,div#l3 input#pass2{opacity: 0.5;border-radius:0px 4px 4px 4px!important;border:1px solid #000000;text-align:center}
+div#l2,div#l3{padding:4px!important}
+div#l1{margin:0 1px -18px;padding: 4px!important;}
+div#l1,div#l2,div#l3{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa;border-radius:15px;height:69px}
+div#l1 button{ background-image: linear-gradient(to left, rgba(255,255,255,0.2), rgba(99,121,125,15));border-radius:0px 5px 0px 5px!important;border:1px solid #ccc!important;border-radius:2px!important;width:98%;height:27px;margin-left: 3px;padding:0 5px}
+div#l2 button{ background-image: linear-gradient(to left, rgba(255,255,255,0.2), rgba(99,121,125,15));border-radius:0px 5px 0px 5px!important;width:70%}
+
+
+ul.nav.nav-tabs>li>a{border:none;text-align:center;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}
+
+ul.nav.nav-tabs>li.active>a{background-image: linear-gradient(to right, rgba(226,223,223,0.8), rgba(255,255,255,20));border-radius:30px;}
+ul.nav.nav-tabs>li{background-image: linear-gradient(to right, rgba(226,223,223,0.8), rgba(255,255,255,20));border-radius:30px!important;width:32%;margin:0 5px 0 0;}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{ background-image: linear-gradient(to right, rgba(255,255,255,0.3), rgba(226,223,223,15));float:right;margin-right:-26%;padding:0 2px 1px;border-radius:8px;width:23px;text-align:center;}
+.nav>li>a:focus,.nav>li>a:hover{color:#3E6778}
+ul.nav.nav-tabs{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa!important;background-color:#e2dfdf!important;margin-top:2px;float:right;width:100%;padding:3px 1px;border-radius:15px!important;}
+
+
+iv#tlogins::-webkit-scrollbar{width:0px;height:10px}
+
+div#tlogins .fl.mini.u-msg{margin-top:3px!important;border-radius:3px!important}
+a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}
+
+
+button.btn.fr.btn-success.fa.fa-refresh{display:none}
+a.label.label-primary.fl img.fl{display:none}
+#des22{text-align:center!important;font-size:100%!important}
+.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#3E6778}
+.fr.borderg{border:0;margin-top:0;height:0}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}
+label.label.label-primary.mini.fl{padding:1px;border-radius:3px!important}
+
+a.label.bg.d-flex.fl {border-radius: 3px!important;margin: 1px 2px;padding: 0px!important;margin-bottom: 2px;text-align: right!important;width: 76%!important;border-radius: 0px 50px 0px 0px!important;border-right-style: solid!important;border-width: 3px!important;}
+ul.nav.nav-tabs {margin-top: 2px;float: right;width: 100%;padding: 3px 1px;height: 30px!important;}
+span.btn.fr.btn-success.fa.fa-refresh {display: none;}
+
+
+
+.sharit{
+background-image: url("https://www.raed.net/img?id=977192");
+background-size: 100%!important;
+border-radius: 20;
+border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa!important;
+}
+
+</style>`).insertBefore('body')
+
+$(`<img src="https://www.raed.net/img?id=1042304" class="" style="width:390px;height:150px;margin-top: 0px;opacity: 0.67;border-radius: 0px 20px 0px 0px;" >
+
+
+<a id="des22" style="height: 29px;width:70px;border-style: dashed #000;float:right;margin:-130px -1.8px 4px;color: #ffffff;transform:matrix(1, 0, 0, 1, -256, 20);background-color: #e55d7b!important; border-radius: 0px 0px 25px 0px;" class="label label label-primary mini fl " target="_blank" class="fl  fa   fa-phone btn btn-warning" href="sub.html">إشتراكات</a>
+<a id="des22" style="height: 29px;width:70px;margin:-124.7px -1.8px 2px; border-radius: 0px 25px 0px 0px;float:right;color:rgb(255, 217, 0);transform:matrix(1, 0, 0, 1, -256, -15);" class="label label label-primary mini fl" class="fl  fa   fa-star btn btn-success" target="_blank" href="rules.html">القوانين</a>
+<a id="des22" style="height: 29px;color: rgb(255 255 255);width:60px;margin:-55px -2px 2px; border-radius: 25px 0px 0px 0px;;transform:matrix(1, 0, 0, 1, 6, -85);background-color: #e55d7b!important;" class="label label label-primary mini fl" target="_blank" class="fl  fa  fa-heartbeat btn btn-danger" href="mobile.html">تطبيق</a>
+<a id="des22" style="height: 29px;width:40px;color: rgb(255 217 11);width:60px;;margin:-60.6px 62px 2px; border-radius: 0px 0px 0px 25px;transform:matrix(1, 0, 0, 1, -58, -50);" class="label label label-primary mini fl" target="_blank" class="fl  fa fa-graduation-cap btn btn-info" href="contact.html">اتصل بنا</a>
+
+
+
+
+`).insertBefore('.nav-tabs');
+
+
+$(`<marquee direction="right" width="100%" class="sharit" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4" style="text-align: center;height: 23px;color: #10384f;font-family: jazeera,FontAwesome;margin-right: -1px;margin-top: 5px;border-radius: 30px;width: 100%;padding: 1px;height:28px;"><img src="https://www.raed.net/img?id=977185"width="20" height="18" class="inta"> في عصر التواصل الحديث عبر الإنترنت، أصبحت وسائل التواصل الاجتماعي جزءًا لا يتجزأ من حياتنا اليومية. يلجأ الكثيرون إلى  الدردشة للتواصل مع الأصدقاء ، وللتعبير عن أفكارهم ومشاعرهم ، سنتحدث عن شات "لهفه" وكيف يمكن أن يكون مكانًا مثاليًا لبدء محادثات مميزة والتواصل مع الأشخاص الذين تحبهم. فـ مرحباً بكم ،   <img src="https://www.raed.net/img?id=977185"width="20" height="">
+</marquee></div>`).insertBefore('.bg.mini.fl');
+
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<style>
+#loginstat {
+border-radius: 0px 0px 0px 50px!important;
+  margin-left: 270px!important;
+height: 25px;
+min-width: 120px;
+border-bottom:2px solid;
+margin-bottom:2px;
+border-top:2px solid;
+margin-top:2px;
+border-color: #bababa!important;
+margin-top: -322px!important;
+border-left-style: solid;
+}
+#s1{
+  margin-right:284px!important;
+border-radius:  0px 30px 30px 0px!important;
+min-width: 44px!important;
+height: 24.7px;
+}
+</style>`).insertBefore('body')
+
+$("#u1,#u3").attr("maxlength",4) 
+minL=10;   
