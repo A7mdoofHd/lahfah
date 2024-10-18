4e1b6e6002c5e8d0bc99b62430766eb53f4c545d
A7mdoofHd <a7med.mads@gmail.com>
2024-10-18 22:27:43 +0300

    Create beomoci.js

---
+++ beomoci.js
@@ -0,0 +1,368 @@
+var rid='192761ed3a4x171da7c1-x-ps7q15';
+$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
+background-size: 220% auto;border-radius:  10px 0 10px!important;border: 1px solid #0000;
+}`);
+$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
+background-image: url(https://www.raed.net/img?id=1053707); background-size: cover;color: #666666;
+}`);
+
+
+$(`<style>
+.nosel.d-flex.room.r192761ed3a4x171da7c1-x-ps7q15 .fitimg.u-pic.borderg {
+  letter-spacing: 1px;
+  background-size: 108% 108%;
+  -webkit-text-fill-color: white;
+  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+}
+
+
+.nosel.d-flex.room.r192761ed3a4x171da7c1-x-ps7q15 .corner.fa.fa-user.label.uc.fa-microphone.label-danger {
+  letter-spacing: 1px;
+  -webkit-text-fill-color: #000000e1;
+  border-radius: 15px 15px 15px 15px !important;
+  height: 27px;
+  margin-top: 40px !important;
+  margin-bottom: 0px !important;
+  background-color: #d9534f00;
+  text-align: left!important;
+  margin-left: 45px!important;
+}
+
+.nosel.d-flex.room.r192761ed3a4x171da7c1-x-ps7q15.bord {border-inline: 0px solid #4c121e !important;}
+
+.nosel.d-flex.room.r192761ed3a4x171da7c1-x-ps7q15 .dots.mini.u-msg {
+    text-align: center;
+    margin-top: 53px !important;
+	margin-left: 11px;
+	-webkit-text-fill-color: transparent;
+	animation: textclip 2s linear infinite;
+	background-image: url(https://up6.cc/2023/02/167536001950611.gif);
+	-webkit-background-clip: text;
+	color: transparent !important;
+	background-size: 90% 100%;
+	opacity:0;
+}
+
+.nosel.d-flex.room.r192761ed3a4x171da7c1-x-ps7q15 {
+    background-image: url(https://www.raed.net/img?id=1053761);
+    background-size: cover;
+    border-radius: 15px 15px 15px 15px;
+    display: inline-block;
+}
+
+.nosel.d-flex.room.r192761ed3a4x171da7c1-x-ps7q15 .fitimg.u-pic.borderg {
+  border-radius: 100px;
+  border: 2px solid #cc8321;
+  letter-spacing: 1px;
+  background-size: 108% 108%;
+  -webkit-text-fill-color: white;
+  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+  min-width: 62px !important;
+  width: 62px !important;
+  min-height: 60px !important;
+  max-height: 60px !important;
+  display: none;
+}
+
+.nosel.d-flex.room.r192761ed3a4x171da7c1-x-ps7q15 .u-topic.dots {
+  text-transform: uppercase;
+  -webkit-text-fill-color: #dda25568;
+  animation: textclip 2s linear infinite;
+  display: inline-block;
+  font-size: 22px !important;
+  background-image: url(https://up6.cc/2023/01/167391087161381.gif);
+  -webkit-background-clip: text;
+  color: transparent !important;
+  background-size: 100% 100%;
+  margin-top: 10px;
+  display: none;
+}
+
+@keyframes textclip {to {background-position: 120% center;}}</style>`).insertBefore('body');
+
+
+$(`<style>#mic4 {margin-right: 8px !important;}
+#mic.oss .mic {
+    border: 0px solid;
+    box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 5px rgb(0 0 0), 0 0 0 0px #000000;
+    margin: 3px;
+    margin-top: 1px;
+	height: 48px;
+}
+@keyframes huerotate {
+    0% {
+        filter: hue-rotate(0deg);
+    }
+    100% {
+        filter: hue-rorate(360deg);
+    }
+}</style>`).insertBefore('body');
+
+$("style").last().append(`
+#upro.oss .border {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #bfbfbf;
+}
+
+#upro.oss .modal-header.label-primary {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #ecdce4;
+  background-color: #b2b2b2!important;
+  background-image: url(https://www.raed.net/img?id=1053707);
+  background-size: cover;
+}
+
+#d2.oss .uzr .fitimg.u-pic {
+height: 52px !important;
+border: 1px solid;
+border-radius: 10px 10px 10px;
+filter: hue-rotate(360deg);
+box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+margin-top: 2px;
+margin-right: 2px;
+}
+
+#users.oss .fitimg.u-pic {
+border: 1px solid;
+border-radius: 10px 10px 10px;
+box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+margin-top: 2px;
+margin-right: 2px;
+border-radius: 10px 10px 10px;
+margin-left: -12px;
+height: 52px;
+}
+
+#d0.oss {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #ecdce41c;
+  background-image: url(https://www.raed.net/img?id=1053707);
+  background-size: cover;
+}
+
+#d2.oss .label-primary{background-color:#b77c9a!important}#d2.oss .bg{background-color:#b77c9a!important}#d2.oss .uzr.d-flex.mm.hmsg{border-radius: 10px 10px 10px;background-color: #3426231f!important;}#dpnl.oss{background-size: 100%;background-image: url();background-color: #b2b2b2;  }#dpnl.oss .light{background-color:#ecdce4!important;background-image:linear-gradient(225deg,#dfdfdf 0%,#f7f7f7 29%,#f4f4f4 67%,#dbdbdb 100%);}#d0.oss .nosel.fl.bg.{background-color:#b77c9a!important;  }#users.oss span.uhash{text-transform:uppercase;background-image:linear-gradient(-225deg,#7b7b7b 0,#737373 29%,#ebebeb 67%,#3600ff 100%);background-size:220% auto;-webkit-background-clip:text;-webkit-text-fill-color:transparent;animation:2s linear infinite textclip;display:inline-block}
+#users.oss .mini.u-msg{text-align:center;}#dpnl.oss.bg{background-image:linear-gradient(225deg,#ce96b3 0%,#d091b1 29%,#eec6da 67%,#b77c9a 100%) */}#rooms.oss .corner.fa.fa-user.label.uc.fa-microphone.label-danger{border-radius: 10px 10px 10px!important}
+#rooms.oss .label-primary {
+    border-radius:  10px 10px 10px!important;
+    background-color: #4b4831e6!important;
+    border: 1px solid #efefef!important;
+}
+
+#users.oss img.ustat {
+    width: 13px !important;
+    height: 13px !important;
+    min-height: 0%!important;
+    margin-right: 0;
+    z-index: 999999;
+    border-radius: 5px 5px 5px;
+    margin-left: 1px;
+    margin-top: 1px;
+    border: 1px solid #fff;
+    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #f0c01f;
+}
+
+#users.oss img.co{border-radius:  10px 10px 10px!important;transform:rotate(20deg);margin-top:2px!important}
+#users.oss .nosel.ninr.fl.uzr.label.bg {
+    border-radius:  10px 10px 10px!important;
+    background-color: #b2b2b2;
+	color: #ffffff;
+	background-image: url(https://www.raed.net/img?id=1053707);
+    background-size: cover;
+}
+
+#d0.oss .label-primary {
+    border: 1px solid #4a4646 !important;
+    border-radius: 10px 10px 10px!important;
+    background-image: url();
+    color: #ffffff;
+    background-size: 100%;
+}
+
+#settings.oss .label-primary {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #ecdce4;
+  background-color: #b2b2b2!important;
+  background-image: url(https://www.raed.net/img?id=1053707);
+  background-size: cover;
+  color: #ffffff;
+}
+
+#settings.oss .btn-primary {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #ecdce4;
+  background-color: #b2b2b2!important;
+  background-image: url(https://www.raed.net/img?id=1053707);
+  background-size: cover;
+  color: #ffffff;
+}
+
+#settings.oss .bg-primary{background-color:#9d8980!important}#d0.oss .btn-primary{background-color:#9d8980!important}
+#users.oss .tbox {
+    border-radius:  10px 10px 10px!important;
+    background-color: #52443e;
+    border: 1px solid #efefef;
+}
+
+#room.oss #mic{
+background-image: url(https://www.raed.net/img?id=1053707);
+background-size: cover;
+border-radius:  10px 10px 10px!important;
+}
+
+#d0.oss .nosel.fl .bg{background-image:linear-gradient(225deg,#ce96b3 0%,#d091b1 29%,#eec6da 67%,#b77c9a 100%)}#d0.oss .bg-primary{background-image:linear-gradient(225deg,#ce96b3 0%,#d091b1 29%,#eec6da 67%,#b77c9a 100%)}#d2.oss .label-primary{background-image:linear-gradient(225deg,#ce96b3 0%,#d091b1 29%,#eec6da 67%,#b77c9a 100%)}
+#room.oss .btn-primary {
+    border-radius:  10px 10px 10px!important;
+    border: 1px solid #f1f1f1;
+    background-image: url();
+    background-size: cover;
+    color: #ffffff;
+    font-size: 14px !important;
+}
+
+#d2.oss{background-color:#fff !important;}
+#room.oss .tablebox.d-flex.footer.fl.light {
+    border-radius:  10px 10px 10px!important;
+    border: 1px solid #ecdce4;
+    background-color: #52443e;
+}
+
+#upro.oss .light.fl.pro.break {
+    background-image: linear-gradient(225deg,#dfdfdf 0%,#f7f7f7 29%,#f4f4f4 67%,#dbdbdb 100%);
+    background-image: url();
+    background-size: 100%;
+}
+
+#mic.oss #muteall {
+    border-radius: 10px 10px 10px;
+    margin-top: 1px;
+    border: 1px solid;
+    clip-path: inset(0px round 15px 0px 15px 0px);
+    box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+}
+
+#wall.oss .fitimg.u-pic.borderg {
+height: 52px !important;
+border: 1px solid;
+border-radius: 10px 10px 10px;
+box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+margin-top: 2px;
+margin-right: 2px;
+}
+
+#rooms.oss .fitimg.u-pic.borderg{
+height: 52px !important;
+border: 1px solid;
+border-image: linear-gradient(45deg, gold, deeppink) 0;
+filter: hue-rotate(360deg);
+box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+}
+.corner.border.label.label-primary.oss {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #ecdce4;
+  background-image: url(https://www.raed.net/img?id=1053707);
+  background-size: cover;
+}
+.modal-header.label-primary.oss {
+	border-radius:  10px 10px 10px!important;
+    border: 1px solid #ecdce4;
+    background-color: #b2b2b2!important;
+	background-image: url(https://www.raed.net/img?id=1053707);
+    background-size: cover;
+}
+.head.d-flex.nosel.bg.fl.oss{
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #f1f1f1;
+  background-image: url(https://www.raed.net/img?id=1054389);
+  color: #ffffff;
+
+}
+.rsave.btn.btn-primary.fr.oss, .fa.fa-send.sndpm.fl.btn.btn-primary.oss, .fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary.oss, .fr.fa.fa-share-alt.sndfilebc.fl.btn.btn-primary.oss, .bdel.corner.btn.minix.btn-primary.fa.fa-times.oss, .fa.fa-send.sndbc.fl.btn.btn-primary.oss {
+   
+  border-radius:  10px 10px 10px!important;
+    border: 1px solid #f1f1f1;
+    background-image: url();
+    color: #ffffff;
+}
+.btn.btn-primary.u-nickc.fr.fa.fa-save.oss {
+    border-radius:  10px 10px 10px!important;
+    border: 1px solid #e7e7e7;
+    background-image: url();
+    color: #ffffff;
+}
+.label.fl.label-primary.oss {
+    border-radius:  10px 10px 10px!important;
+    border: 1px solid #e7e7e7;
+    background-image: url();
+    color: #ffffff;
+}
+#rooms.oss{background-color:#ecdce4!important;background-image:linear-gradient(225deg,#e3c4d3 0%,#fdedf5 29%,#f4f4f4 67%,#f9dbea 100%)}#settings.oss{background-color:#ecdce4!important;background-image:linear-gradient(225deg,#e3c4d3 0%,#fdedf5 29%,#f4f4f4 67%,#f9dbea 100%)}#d2bc.oss{background-color:#ecdce4!important;background-image:linear-gradient(225deg,#e3c4d3 0%,#fdedf5 29%,#f4f4f4 67%,#f9dbea 100%)}
+input.oss {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #ecdce4;
+}
+.dots.oss {
+  border-radius:  10px 10px 10px!important;
+}
+.tc.oss {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #ecdce4;
+}
+.fl.u-pic.fitimg.oss {
+  border: 1px solid;
+
+clip-path: inset(0px round 0px 50px 0px 50px);
+animation: huerotate 2s infinite linear;
+box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 10px rgb(0 0 0), 0 0 0 0px #000;
+}
+.fl.borderg.oss {
+  border-radius:  10px 10px 10px!important;
+  border: 1px solid #bfbfbf;
+}
+.uhtml.oss {
+  border-radius: 10px 10px 10px;
+}
+
+#users.oss .inroom .fitimg.u-pic {
+border: 1px solid;
+box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+margin-top: 2px;
+margin-right: 2px;
+margin-left: -12px;
+height: 52px;
+border-radius: 10px 10px 10px;
+}
+
+#room.oss {
+   background-color: white;
+}
+
+button.blike.corner.btn.minix.btn-danger.fa.fa-heart.oss {
+   border-radius: 10px 10px 10px;
+}
+button.border.btn.label.label-success.fl.fa.fa-plus.oss {
+   border-radius: 10px 10px 10px;
+}
+button.rsave.btn.btn-primary.fl.oss {
+   background-image: url(https://www.raed.net/img?id=1053707);
+   background-size: cover;
+   border-radius: 10px 10px 10px;
+}
+
+
+`);
+var oss = false;
+setInterval(function () {
+    if (myroom == '192761ed3a4x171da7c1-x-ps7q15') {
+        if (oss == false) {
+            oss = true;
+                $('#d0, #users, #d2, #mic, #settings, #rooms, #room, #d0, #chats, #d2bc, #muteall, #wall, #dpnl, #upro').addClass('oss');
+		$('input, button.rsave.btn.btn-primary.fl, button.border.btn.label.label-success.fl.fa.fa-plus, button.blike.corner.btn.minix.btn-danger.fa.fa-heart, .fl.borderg, .fl.u-pic.fitimg, .dots, .tc, .label.fl.label-primary, .btn.btn-primary.u-nickc.fr.fa.fa-save, .modal-header.label-primary, .corner.border.label.label-primary,.head.d-flex.nosel.bg.fl, .rsave.btn.btn-primary.fr, .fa.fa-send.sndpm.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfilebc.fl.btn.btn-primary, .bdel.corner.btn.minix.btn-primary.fa.fa-times, .fa.fa-send.sndbc.fl.btn.btn-primary').addClass('oss');
+        }
+    } else {
+        if (oss == true) {
+            oss = false;
+                $("#d0, #users, #d2, #mic, #settings, #rooms, #room, #d0, #chats, #d2bc, #muteall, #wall, #dpnl, #upro").removeClass('oss');
+		$('input, button.rsave.btn.btn-primary.fl, button.border.btn.label.label-success.fl.fa.fa-plus, button.blike.corner.btn.minix.btn-danger.fa.fa-heart, .fl.borderg, .fl.u-pic.fitimg, .dots, .tc, .label.fl.label-primary, .btn.btn-primary.u-nickc.fr.fa.fa-save, .modal-header.label-primary, .corner.border.label.label-primary, .head.d-flex.nosel.bg.fl, .rsave.btn.btn-primary.fr, .fa.fa-send.sndpm.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfilebc.fl.btn.btn-primary, .bdel.corner.btn.minix.btn-primary.fa.fa-times, .fa.fa-send.sndbc.fl.btn.btn-primary').removeClass('oss');
+            } 
+        } 
+    }, 10);
